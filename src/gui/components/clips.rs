073be0a09ed55dce66e7aa3b77b11c6152e54f3b
Ryan <ryanbays@icloud.com>
2026-01-26 19:18:37 +0000

    Added os specific code and better multithreading.

--- src/gui/components/clips.rs
+++ src/gui/components/clips.rs
@@ -25,7 +25,7 @@ impl ClipManager {
                 ui.heading("Audio Clips");
                 for (i, clip) in self.clips.iter().enumerate() {
                     let id = egui::Id::new(format!("audio_clip_{}", i));
-                    let label = egui::Button::selectable(false, (i + 1).to_string());
+                    let label = egui::Button::selectable(false, clip.file_path.to_string_lossy());
                     let payload = clip.clone();
                     ui.dnd_drag_source(id, payload, |ui| {
                         ui.add(label);
